91f1551dcb63d1108dbd503295828aafef4e279c
Narek Vardanjan <vardanar@fit.cvut.cz>
2022-06-29 16:19:20 +0200

    Add zerops benchmark

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,6 +44,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
+        if (env('ZEROPS_BENCHMARK')) {
+            return;
+        }
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
